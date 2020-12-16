a4bfc7db5fe79b04b8dbe20720a411fd2f9b7e9f
BK | Mobile Apps <developer+git-commit@bennokress.de>
2020-12-16 12:50:41 +0100

    Initial Commit

---
+++ SCHelper/SCHelperApp.swift
@@ -0,0 +1,19 @@
+//
+//  SCHelperApp.swift
+//  SCHelper
+//
+//  Created by Benno Kress on 16.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SCHelperApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+
+}
---
+++ SCHelper/Screens/HomeScreen.swift
@@ -0,0 +1,16 @@
+//
+//  HomeScreen.swift
+//  SCHelper
+//
+//  Created by Benno Kress on 16.12.20.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+
+    var body: some View {
+        Text("This app only provides Shortcut Actions for now!")
+    }
+
+}
