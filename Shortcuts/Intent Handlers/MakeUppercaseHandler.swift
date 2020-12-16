c440ab11b062c262caa7ead17102da62d78f1366
BK | Mobile Apps <developer+git-commit@bennokress.de>
2020-12-16 15:30:22 +0100

    Add MakeUppercase Intent

---
+++ Shortcuts/Intent Handlers/MakeUppercaseHandler.swift
@@ -0,0 +1,29 @@
+//
+//  MakeUppercaseHandler.swift
+//  Shortcuts
+//
+//  Created by Benno Kress on 16.12.20.
+//
+
+import Intents
+
+class MakeUppercaseIntentHandler: NSObject, MakeUppercaseIntentHandling {
+
+    func handle(intent: MakeUppercaseIntent, completion: @escaping (MakeUppercaseIntentResponse) -> Void) {
+        if let inputText = intent.text {
+            let uppercaseText = inputText.uppercased()
+            completion(MakeUppercaseIntentResponse.success(result: uppercaseText))
+        } else {
+            completion(MakeUppercaseIntentResponse.failure(error: "The entered text was invalid"))
+        }
+    }
+
+    func resolveText(for intent: MakeUppercaseIntent, with completion: @escaping (MakeUppercaseTextResolutionResult) -> Void) {
+        if let text = intent.text, !text.isEmpty {
+            completion(MakeUppercaseTextResolutionResult.success(with: text))
+        } else {
+            completion(MakeUppercaseTextResolutionResult.unsupported(forReason: .noText))
+        }
+    }
+
+}
